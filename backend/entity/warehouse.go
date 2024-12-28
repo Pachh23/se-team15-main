78d24b1d1ebdb345bb4db79364005d52263cc111
Pachh23 <wamagant29@gmail.com>
2024-12-28 18:05:44 +0700

    test

--- backend/entity/warehouse.go
+++ backend/entity/warehouse.go
@@ -6,17 +6,20 @@ import (
 
 type Warehouses struct {
 	gorm.Model
-	WarehouseName     string             `gorm:"unique;not null" json:"warehouse_name"`
-	WarehouseTypeID   uint               `json:"warehouse_type_id"`
-	Capacity          float64            `json:"capacity"` // หน่วย: m³ (ลูกบาศก์เมตร)
-	WarehouseStatusID uint               `json:"warehouse_status_id"`
-	Address           string             `json:"address"`
-	Zipcode           string             `json:"zipcode"`
-	ProvinceID        uint               `json:"province_id"`
-	Province          *Provinces         `gorm:"foreignKey: ProvinceID" json:"province"`
-	WarehouseType     *WarehouseTypes    `gorm:"foreignKey: WarehouseTypeID" json:"warehouse_type"`
-	WarehouseStatus   *WarehouseStatuses `gorm:"foreignKey: WarehouseStatusID" json:"warehouse_status"`
+	WarehouseName string  `valid:"required~WarehouseName is required" gorm:"unique;not null" json:"warehouse_name"`
+	Capacity      float64 `valid:"required~Capacity is required, positive~Capacity must be positive" json:"capacity"`
+	Address       string  `valid:"required~Address is required" json:"address"`
+	Zipcode       string  `valid:"required~Zipcode is required, stringlength(5|5)" json:"zipcode"`
 
-	Employees []Employee `gorm:"foreignKey:WarehouseID"`
-	Location  []Location `gorm:"foreignKey:WarehouseID"`
+	ProvinceID uint      `valid:"required~Province is required" json:"province_id"`
+	Province   Provinces `gorm:"foreignKey: ProvinceID" json:"province"`
+
+	WarehouseTypeID uint           `valid:"required~WarehouseType is required" json:"warehouse_type_id"`
+	WarehouseType   WarehouseTypes `gorm:"foreignKey: WarehouseTypeID" json:"warehouse_type"`
+
+	WarehouseStatusID uint              `valid:"required~WarehouseStatus is required" json:"warehouse_status_id"`
+	WarehouseStatus   WarehouseStatuses `gorm:"foreignKey: WarehouseStatusID" json:"warehouse_status"`
+
+	//Employees []Employee `gorm:"foreignKey:WarehouseID"`
+	//Location  []Location `gorm:"foreignKey:WarehouseID"`
 }
---
+++ backend/test/warehouse_test.go
@@ -0,0 +1,114 @@
+package unit
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/aprbq/se-team15/entity"
+	"github.com/asaskevich/govalidator"
+	. "github.com/onsi/gomega"
+)
+
+func TestWarehouse(t *testing.T) {
+
+	govalidator.CustomTypeTagMap.Set("positive", govalidator.CustomTypeValidator(func(i interface{}, context interface{}) bool {
+		switch v := i.(type) {
+		case float64:
+			return v > 0
+		}
+		return false
+	}))
+
+	g := NewGomegaWithT(t)
+
+	t.Run(`Check all required fields for Warehouse`, func(t *testing.T) {
+		warehouse := entity.Warehouses{
+			WarehouseName:     "Warehouse A",
+			WarehouseTypeID:   3,
+			Capacity:          500, // หน่วย: m³ (ลูกบาศก์เมตร)
+			WarehouseStatusID: 1,
+			Address:           "123/4 Sukhumvit 22, Khlong Tan Nuea, Watthana",
+			Zipcode:           "10110",
+			ProvinceID:        3,
+		}
+
+		ok, err := govalidator.ValidateStruct(warehouse)
+
+		g.Expect(ok).To(BeTrue())
+		g.Expect(err).To(BeNil())
+
+	})
+
+	t.Run(`WarehouseName is required`, func(t *testing.T) {
+		warehouse := entity.Warehouses{
+			WarehouseName:     "", // ผิดตรงนี้
+			WarehouseTypeID:   3,
+			Capacity:          500,
+			WarehouseStatusID: 1,
+			Address:           "123/4 Sukhumvit 22, Khlong Tan Nuea, Watthana",
+			Zipcode:           "10110",
+			ProvinceID:        3,
+		}
+
+		ok, err := govalidator.ValidateStruct(warehouse)
+
+		g.Expect(ok).NotTo(BeTrue())
+		g.Expect(err).NotTo(BeNil())
+		g.Expect(err.Error()).To(Equal("WarehouseName is required"))
+	})
+
+	t.Run(`Capacity must be positive`, func(t *testing.T) {
+		warehouse := entity.Warehouses{
+			WarehouseName:     "Warehouse A",
+			WarehouseTypeID:   3,
+			Capacity:          -500, // ผิดตรงนี้
+			WarehouseStatusID: 1,
+			Address:           "123/4 Sukhumvit 22, Khlong Tan Nuea, Watthana",
+			Zipcode:           "10110",
+			ProvinceID:        3,
+		}
+
+		ok, err := govalidator.ValidateStruct(warehouse)
+
+		g.Expect(ok).NotTo(BeTrue())
+		g.Expect(err).NotTo(BeNil())
+		g.Expect(err.Error()).To(ContainSubstring("positive"))
+	})
+
+	t.Run(`Address is required`, func(t *testing.T) {
+		warehouse := entity.Warehouses{
+			WarehouseName:     "Warehouse A", // ผิดตรงนี้
+			WarehouseTypeID:   3,
+			Capacity:          500,
+			WarehouseStatusID: 1,
+			Address:           "",
+			Zipcode:           "10110",
+			ProvinceID:        3,
+		}
+
+		ok, err := govalidator.ValidateStruct(warehouse)
+
+		g.Expect(ok).NotTo(BeTrue())
+		g.Expect(err).NotTo(BeNil())
+		g.Expect(err.Error()).To(Equal("Address is required"))
+	})
+
+	t.Run(`Zipcode check 5 digit`, func(t *testing.T) {
+		warehouse := entity.Warehouses{
+			WarehouseName:     "Warehouse A", // ผิดตรงนี้
+			WarehouseTypeID:   3,
+			Capacity:          500,
+			WarehouseStatusID: 1,
+			Address:           "123/4 Sukhumvit 22, Khlong Tan Nuea, Watthana",
+			Zipcode:           "101100", // ผิดตรงนี้ มี 6 ตัว
+			ProvinceID:        3,
+		}
+
+		ok, err := govalidator.ValidateStruct(warehouse)
+
+		g.Expect(ok).NotTo(BeTrue())
+		g.Expect(err).NotTo(BeNil())
+		g.Expect(err.Error()).To(Equal(fmt.Sprintf("zipcode: %s does not validate as stringlength(5|5)", warehouse.Zipcode)))
+	})
+
+}
